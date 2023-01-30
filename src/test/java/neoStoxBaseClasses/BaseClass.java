5679f88258f216c74ddb27fc188fc5f1e61996a8
reshamm <reshammalwade9@gmail.com>
2023-01-30 15:36:22 +0530

    second

--- src/test/java/neoStoxBaseClasses/BaseClass.java
+++ src/test/java/neoStoxBaseClasses/BaseClass.java
@@ -13,6 +13,7 @@ public class BaseClass
 	protected WebDriver driver;
 	public void launchBrowser()
 	{
+		
 	  System.setProperty("webdriver.chrome.driver","D:\\20th_evening\\Selenium\\chromedriver_win32\\chromedriver.exe");
 	  driver=new ChromeDriver();
 	  
